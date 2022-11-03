efbf379a6c4295eb0b2d4377ead8c996daef25ae
slandymani <slandyman9@gmail.com>
2022-11-03 19:27:51 +0200

    add validations; optimize ci

--- internal/service/requests/change_password.go
+++ internal/service/requests/change_password.go
@@ -2,6 +2,7 @@ package requests
 
 import (
 	"encoding/json"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"gitlab.com/distributed_lab/logan/v3/errors"
 	"gitlab.com/tokene/keyserver-svc/resources"
 	"net/http"
@@ -19,5 +20,12 @@ func NewChangePasswordRequest(r *http.Request) (resources.ChangePasswordRequest,
 		return request.Data, errors.Wrap(err, "failed to unmarshal")
 	}
 
-	return request.Data, nil
+	return request.Data, request.validate()
+}
+
+func (r ChangePasswordRequest) validate() error {
+	return validation.Errors{
+		"/data/attributes/wallet_id": validation.Validate(&r.Data.Attributes.WalletId, validation.Required),
+		"/data/attributes/email":     validation.Validate(&r.Data.Attributes.Email, validation.Required),
+	}.Filter()
 }
--- internal/service/requests/create_wallet.go
+++ internal/service/requests/create_wallet.go
@@ -2,6 +2,7 @@ package requests
 
 import (
 	"encoding/json"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"gitlab.com/distributed_lab/logan/v3/errors"
 	"gitlab.com/tokene/keyserver-svc/resources"
 	"net/http"
@@ -19,5 +20,12 @@ func NewCreateWalletRequest(r *http.Request) (resources.CreateWalletRequest, err
 		return request.Data, errors.Wrap(err, "failed to unmarshal")
 	}
 
-	return request.Data, nil
+	return request.Data, request.validate()
+}
+
+func (r CreateWalletRequest) validate() error {
+	return validation.Errors{
+		"/data/attributes/wallet_id": validation.Validate(&r.Data.Attributes.WalletId, validation.Required),
+		"/data/attributes/email":     validation.Validate(&r.Data.Attributes.Email, validation.Required),
+	}.Filter()
 }

583dd2512937a2f259e466dc2cf244c98e54bfc1
slandymani <slandyman9@gmail.com>
2022-11-04 16:05:50 +0200

    add running verification sender

--- internal/service/main.go
+++ internal/service/main.go
@@ -26,6 +26,8 @@ func (s *service) run() error {
 	s.log.Info("Service started")
 	r := s.router()
 
+	go s.runVerificationSender()
+
 	if err := s.copus.RegisterChi(r); err != nil {
 		return errors.Wrap(err, "cop failed")
 	}
